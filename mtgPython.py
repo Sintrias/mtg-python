68bfbdf58adcef8e1daea29027102a7089fe7545
Hans <hsjungcr@gmail.com>
2017-07-18 18:44:52 -0700

    pivot to mtg deck maker

--- mtgPython.py
+++ mtgPython.py
@@ -2,48 +2,70 @@ import random
 import os
 import sys
 
-# modern_card_list = ["Path to Exile", "Thoughtseize", "Serum Visions",
-#              "Fatal Push"]
-# print("#1 played card in modern", modern_card_list[0])
-# print(modern_card_list[1:3])
-#
-# standard_card_list = ["Thraben Inspector", "Gideon, Ally of Zendikar",
-#                       "Negate", "Fetal Push"]
-# all_card_list = [modern_card_list,standard_card_list]
-# print(all_card_list)
-# print("%s"%("#4 most played card in Standard : "),all_card_list[1][3])
-# standard_card_list.append("Harnessed Lightening")
-# standard_card_list.append("Abrade")
-# standard_card_list.insert(5,"Attune with Aether")
-# standard_card_list.remove("Abrade")
-# standard_card_list.sort()
-# standard_card_list.reverse()
-# del standard_card_list[0]
-# all_card_list2 = standard_card_list + modern_card_list
-#
-# print(len(all_card_list2))
-#
-# print(standard_card_list)
-# print(max(all_card_list2))
-# print(min(all_card_list2))
+def is_basic_land(land):
+    land = land.lower()
+    basic_lands = ["mountain", "island", "swamp", "plain", "forest"]
+    for lands in basic_lands:
+        if(land == lands):
+            return True
+        else:
+            return False
+print("Welcome to deck tracker")
+answer = "n"
+deck_list = {}
+sideboard_list = {}
+while(answer == "n"):
+    print("What would you like to name your deck?")
+    deck_name = sys.stdin.readline()
+    print("Would you like to set your deck name as "+ deck_name+ "?(y/n)")
+    answer = sys.stdin.readline().rstrip()
+deck_size = 0
+sideboard_size = 0
+count = 1
+while(deck_size<60):
+    print("Entry number ",count, ". Provide card name:")
+    card = sys.stdin.readline().strip();
+    print(card," is the card you entered is this okay?")
+    answer = sys.stdin.readline().strip()
+    if(answer != "y"):
+        continue
+    print("How many copies would you like to include?")
+    answer = sys.stdin.readline().strip()
+    if (is_basic_land(card)):
+        deck_list[card] = int(answer)
+        count += 1
+        deck_size += deck_list[card]
+    elif (1 <= int(answer) < 5) :
+         deck_list[card] = int(answer)
+         count+=1
+         deck_size+=deck_list[card]
+    else:
+        print("You can only have 1 to 4 copies in your deck")
+        continue
+print("Would you like to add sideboard?(y/n")
+answer = sys.stdin.readline.strip()
+if(answer == 'y'):
+    while(sideboard_size < 15):
+        print("Provide card name:")
+        card = sys.stdin.readline().strip()
+        print("You entered ", card)
+        print("How many copies would you like?")
+        answer = sys.stdin.readline().strip()
+        if(1 <= int(answer) < 5 and sideboard_size+int(answer) < 15) :
+            print("You added ", answer, " copies of ", card)
+            sideboard_list[card] = int(answer)
+            sideboard_size+= int(answer)
 
-deckList = {"Liliana, the Last Hope" : 3, "Distended Mindbender" : 2, "Emrakul, the Promised End" : 1, "Nissa" : "2"}
-
-print(deckList["Liliana, the Last Hope"])
-
-del deckList["Nissa"]
-print(deckList)
-deckList["Nissa, the Vastwood Seer"] = 2
-print(deckList)
-print(len(deckList))
-print(deckList.get("Nissa, the Vastwood Seer"))
-
-current_deck_size = 0
-
-for key, value in deckList.items():
-    current_deck_size += value
-
-if current_deck_size < 60 :
-    print("You have %i cards in your deck. Your deck must have at least 60 cards"%(current_deck_size))
-else :
-    print("Your deck have enough cards to be legal deck")
+        else:
+            print("You can have up to 15 sideboard cards and must be within 1 to 4 cards")
+        print("Add more?(y/n)")
+        answer = sys.stdin.readline().strip()
+        if(answer.lower() == "n"):
+            break;
+print("Your deck name:", deck_name)
+print("Main deck list: ")
+for keys, val in deck_list:
+    print(keys, ":", val)
+print("Your side board list: ")
+for keys, val in sideboard_list:
+    print(keys, ":", val)
